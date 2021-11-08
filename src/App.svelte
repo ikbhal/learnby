22d848172440ad3a94dc54031ddb51ba8e407312
Shaik Ikbhal Basha <iqbalforall@gmail.com>
2021-11-08 09:56:28 +0530

    adding few example, tutorial, links, youtube, community, repl for kotlin

--- src/App.svelte
+++ src/App.svelte
@@ -5,6 +5,36 @@
 <h1>Kotlin tutorials</h1>
 
 
+<h3>Kotli Examples</h3>
+<ul>
+	<li><a href='https://medium.com/@tanunprabhu95/to-do-list-application-using-kotlin-using-android-studio-546e74ac75aa'>
+		Kotlin Todo App
+	</a></li>
+	<li>
+		<a href='https://pusher.com/tutorials/live-blog-kotlin-push-notifications/'>
+		Build a live blog in Kotlin with push notifications
+		</a>
+	</li>
+</ul>
+
+<h3>Kotlin Repl</h3>
+<ul>
+	<li><a href='https://replit.com/languages/kotlin'>Kotlin replit.com</a></li>
+</ul>
+
+<h3>Kotlin Youtube</h3>
+<ul>
+	<li> <a href='https://www.youtube.com/watch?v=F9UC9DY-vIU'>Kotlin Course - Tutorial for Beginners</a></li>
+</ul>
+
+<h3>Links</h3>
+<ul>
+	<li> <a href='https://kotlinlang.org/'>Kotlin Lang</a></li>
+	<li> <a href='https://play.kotlinlang.org/'>Kotlin Play</a></li>
+	<li> <a href='https://play.kotlinlang.org/koans/overview'>Kotlin Koans</a></li>
+	<li> <a href='https://kotlinlang.org/community/'>Kotlin Community</a></li>
+</ul>
+
 </main>
 
 <style>
