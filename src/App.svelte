6b60646c5e5ce694a7b944314efd24b8fea56fd2
Shaik Ikbhal Basha <iqbalforall@gmail.com>
2021-11-08 11:20:38 +0530

    moving hard code text to variables

--- src/App.svelte
+++ src/App.svelte
@@ -1,4 +1,70 @@
 <script>
+let examples = [
+	{
+		link: 'https://medium.com/@tanunprabhu95/to-do-list-application-using-kotlin-using-android-studio-546e74ac75aa',
+		title: 'Kotlin Todo App',
+	},
+	{
+		link: 'https://pusher.com/tutorials/live-blog-kotlin-push-notifications/',
+		title: 'Kotlin Blog App',
+	}
+];
+
+let repls = [{
+	link: 'https://learnby.vercel.app/',
+	title: 'Kotlin Replit',
+}];
+
+
+let tutorials = [
+	{
+		link: 'https://beginnersbook.com/2017/12/kotlin-tutorial/',
+		title: 'Beginners Book Kotlin Tutorial'
+	},
+	{
+		link: 'https://www.programiz.com/kotlin-programming',
+		title: 'Programiz Kotlin Tutorial'
+	},
+	{
+		link: 'https://www.w3schools.com/kotlin/index.php',
+		title: 'W3schol Kotlin Tutorial'
+	},
+	{
+		link: 'https://www.tutorialspoint.com/kotlin/index.htm',
+		title: 'Tutorial Point Kotlin Tutorial'
+	},
+	{
+		link: 'https://www.javatpoint.com/kotlin-tutorial',
+		title: 'Java Point Kotlin Tutorial'
+	},
+
+];
+
+let youtubeVideos = [
+	{
+		link:  'https://www.youtube.com/watch?v=F9UC9DY-vIU',
+		title: 'Kotlin Course - Tutorial for Beginners',
+	}
+];
+
+let links = [
+	{
+		link: 'https://kotlinlang.org/',
+		title: 'Kotlin Lang'
+	},
+	{
+		link: 'https://play.kotlinlang.org/',
+		title: 'Kotlin Play'
+	}, 
+	{
+		link: 'https://play.kotlinlang.org/koans/overview',
+		title: 'Kotlin Koans',
+	}, 
+	{
+		link: 'https://kotlinlang.org/community/', 
+		title: 'Kotlin community',
+	}
+];
 </script>
 
 <main>
@@ -7,32 +73,45 @@
 
 <h3>Kotli Examples</h3>
 <ul>
-	<li><a href='https://medium.com/@tanunprabhu95/to-do-list-application-using-kotlin-using-android-studio-546e74ac75aa'>
-		Kotlin Todo App
-	</a></li>
+	{#each examples as example}
 	<li>
-		<a href='https://pusher.com/tutorials/live-blog-kotlin-push-notifications/'>
-		Build a live blog in Kotlin with push notifications
+		<a href={example.link}>
+			{example.title}
 		</a>
 	</li>
+	{/each}
 </ul>
 
 <h3>Kotlin Repl</h3>
 <ul>
-	<li><a href='https://replit.com/languages/kotlin'>Kotlin replit.com</a></li>
+	{#each repls as repl }
+	<li>
+		<a href={repl.link}>{repl.title}</a>
+	</li>
+	{/each}
+</ul>
+
+<h3>Kotlin Tutorial</h3>
+<ul>
+	{#each tutorials as tutorial}
+		<li>
+			<a href={tutorial.link}> {tutorial.title} </a>
+		</li>
+	{/each}
 </ul>
 
 <h3>Kotlin Youtube</h3>
 <ul>
-	<li> <a href='https://www.youtube.com/watch?v=F9UC9DY-vIU'>Kotlin Course - Tutorial for Beginners</a></li>
+	{#each youtubeVideos as yt}
+	<li> <a href={yt.link}>{yt.title}</a></li>
+	{/each}
 </ul>
 
 <h3>Links</h3>
 <ul>
-	<li> <a href='https://kotlinlang.org/'>Kotlin Lang</a></li>
-	<li> <a href='https://play.kotlinlang.org/'>Kotlin Play</a></li>
-	<li> <a href='https://play.kotlinlang.org/koans/overview'>Kotlin Koans</a></li>
-	<li> <a href='https://kotlinlang.org/community/'>Kotlin Community</a></li>
+	{#each links as link}
+	<li> <a href={link.link}> {link.title} </a></li>
+	{/each}
 </ul>
 
 </main>
